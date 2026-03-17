d455b0df71da7f1b30053a28e0e3b3ec5946b27c
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-03-17 11:29:58 -0400

    update index.html and main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+let cat = document.getElementsByClassName('cat');
+let dog = document.getElementsByClassName('dog');
+let cow = document.getElementsByClassName('cow');
 
+let catb = document.getElementsByClassName('catb');
+let dogb = document.getElementsByClassName('dogb');
+let cowb = document.getElementsByClassName('cowb');
+
+catb.addEventListener("click", function() {
+  cat.hidden = false;
+  catb.style.backgroundColor = "lightgreen"
+});
+
+dogb.addEventListener("click", function() {
+  dog.hidden = false;
+  dogb.style.backgroundColor = "lightgreen"
+});
+
+cowb.addEventListener("click", function() {
+  cow.hidden = false;
+  cowb.style.backgroundColor = "lightgreen"
+});
