4d2bcfea9564931ddf062e18cea3b1f69e8edf8d
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-03-17 11:38:17 -0400

    update main.js

--- js/main.js
+++ js/main.js
@@ -6,17 +6,36 @@ let catb = document.getElementsByClassName('catb');
 let dogb = document.getElementsByClassName('dogb');
 let cowb = document.getElementsByClassName('cowb');
 
+let hidecat = document.getElementById('hidecat');
+let hidedog = document.document.getElementById('hidedog')
+let hidecow = document.document.getElementById('hidecow')
+
 catb.addEventListener("click", function() {
   cat.hidden = false;
-  catb.style.backgroundColor = "lightgreen"
+  catb.style.backgroundColor = "lightgreen";
 });
 
 dogb.addEventListener("click", function() {
   dog.hidden = false;
-  dogb.style.backgroundColor = "lightgreen"
+  dogb.style.backgroundColor = "lightgreen";
 });
 
 cowb.addEventListener("click", function() {
   cow.hidden = false;
-  cowb.style.backgroundColor = "lightgreen"
+  cowb.style.backgroundColor = "lightgreen";
+});
+
+hidecat.addEventListener("click", function() {
+  cat.hidden = true;
+  catb.style.backgroundColor = "white";
+});
+
+hidedog.addEventListener("click", function() {
+  dog.hidden = true;
+  dogb.style.backgroundColor = "white";
+});
+
+hidecow.addEventListener("click", function() {
+  cow.hidden = true;
+  cowb.style.backgroundColor = "white";
 });
